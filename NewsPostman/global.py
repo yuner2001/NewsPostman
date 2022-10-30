e00951eb1a014d82cf089ac0d3217ed1fd216b71
yuner2001 <3298520845@qq.com>
2022-10-30 17:52:37 +0800

    项目框架

---
+++ NewsPostman/global.py
@@ -0,0 +1,16 @@
+
+
+class Paper:
+    def __init__(self) -> None:
+        pass
+
+    def build(self) -> str:
+        return ''
+
+    def toPDF(self,):
+        pass
+
+    def clear(self,):
+        pass
+    
+paper = Paper()
---
+++ NewsPostman/spiders/baidu_spider.py
@@ -0,0 +1,23 @@
+import scrapy
+from NewsPostman.items import NewspostmanItem
+
+
+class BaiduSpider(scrapy.Spider):
+    name = "baidu"
+    start_urls = [
+        'https://top.baidu.com/board?tab=realtime'
+    ]
+
+    def parse(self, response):
+        page = response.url.split("/")[-2]
+        filename = f'Baidu.html'
+        news_title_list = response.xpath('//div[@class="c-single-text-ellipsis"]/text()').getall()
+        for news_title in news_title_list:
+            news_title = news_title[:2] # baidu title begin with two space
+            yield NewspostmanItem(
+                title = news_title,
+                url = '',
+                hot_index = 0,
+                category = '',
+                descrip = ''
+            )
